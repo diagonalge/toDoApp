81b127a7075d362a21cf1cbe2177e7b1e31adf63
Hamza <diagonalge@gmail.com>
2022-08-22 23:40:43 +0500

    cicd commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ['Buy Food', 'Cook Food', 'Eat Food'];
+let items = ['CICD Pipeline example', 'Cook Food', 'Eat Food'];
 let workItems = [];
 
 app.set('view engine', 'ejs');
